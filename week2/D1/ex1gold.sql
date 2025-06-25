61ad77aa003abaa7e5fdea508f9562d5536f78b5
Mooka <47282413+mooka007@users.noreply.github.com>
2025-06-25 10:34:34 +0100

    sql

---
+++ week2/D1/ex1gold.sql
@@ -0,0 +1,16 @@
+SELECT first_name, last_name, birth_date 
+FROM students
+ORDER BY last_name ASC
+LIMIT 4;
+
+SELECT first_name, last_name, birth_date 
+FROM students
+ORDER BY birth_date DESC
+LIMIT 1;
+
+
+SELECT first_name, last_name, birth_date 
+FROM students
+OFFSET 2
+LIMIT 3;
+
