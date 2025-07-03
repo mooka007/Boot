d1390750ab12a8296b0aff4662981b7c3225a8e8
Mooka <47282413+mooka007@users.noreply.github.com>
2025-07-03 16:59:33 +0100

    Update ex3.js

--- week3/d2/ex3.js
+++ week3/d2/ex3.js
@@ -1,6 +1,21 @@
 
 const prompt = require("prompt-sync")();
 
+
+// let input = parseInt(prompt("Veulliez entrer un nombre :"));
+// //Verification du type
+console.log("Type avant conversin ", typeof input);
+// Convertir en nombre :
+let number = Number(input);
+
+//verification si la conversion est reussie :
+if (!isNaN(input)) {
+  console.log("Merci ! vous entre le nombre :", input);
+  console.log("Type après conversion :", typeof input); // "number"
+} else {
+  console.log("Ce que vous avez entré n'est pas un nombre valide.");
+}
+
 let num = parseInt(prompt("Enter a number: "));
 
 while (num < 10) {
